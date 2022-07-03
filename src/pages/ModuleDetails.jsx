6bb833f59045d5f81b9de834144e1a3af0abde47
ma-foyez <mafoyez.bd@gmail.com>
2022-07-03 19:18:53 +0600

    updated

--- src/pages/ModuleDetails.jsx
+++ src/pages/ModuleDetails.jsx
@@ -3,13 +3,13 @@ import { useParams } from 'react-router-dom';
 import MainLayout from '../layouts/MainLayout';
 
 const ModuleDetails = () => {
-    
+
     let params = useParams();
 
     return (
         <MainLayout>
             <div className="content_inner">
-                <h3>This is {params.moduleName} Page</h3>
+                <h2 className='display-6 fw-bold text-uppercase'>Welcome TO {params.moduleName} </h2>
             </div>
         </MainLayout>
     );
