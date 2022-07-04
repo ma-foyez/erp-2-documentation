f60ea17bbe568355ab615e6eb19b8b1b2f1bbe1a
ma-foyez <mafoyez.bd@gmail.com>
2022-07-04 13:17:55 +0600

    menu link add

--- src/assets/data/SidebarMenuList.js
+++ src/assets/data/SidebarMenuList.js
@@ -7,25 +7,45 @@ export const menuList = [
     },
     {
         id: "02",
-        menuName: "Menu - 2",
+        menuName: "Inventory",
         icon: "fa-solid fa-satellite-dish",
-        link: "/getting-started",
+        link: "/module/inventory-management",
         subMenu: [
             {
-                menuName: "Sub-Menu - 1",
-                link: "/submenu-1",
-            },
-            {
-                menuName: "Sub-Menu - 2",
-                link: "/submenu-3",
+                menuName: "Department",
+                link: "/module/inventory-management/department",
+                subSubMenu: [
+                    {
+                        menuName: "Department Entry",
+                        link: "/module/inventory-management/department-entry",
+                    },
+                    {
+                        menuName: "Department Item Received",
+                        link: "/module/inventory-management/department-item-received",
+                    },
+                    {
+                        menuName: "Department Requisition List",
+                        link: "/module/inventory-management/department-requisition-list",
+                    },
+                    {
+                        menuName: "Department Received",
+                        link: "/module/inventory-management/department-received",
+                    },
+                    {
+                        menuName: "Department Requisition",
+                        link: "/module/inventory-management/department-requisition",
+                    },
+
+                ]
             },
         ]
+
     },
     {
-        id: "02",
-        menuName: "Menu - 2",
+        id: "03",
+        menuName: "HRM Management",
         icon: "fa-solid fa-satellite-dish",
-        link: "/getting-started",
+        link: "/module/hrm-management",
         subMenu: [
             {
                 menuName: "Sub-Menu - 1",
